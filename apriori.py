c24901878efb39960b160ec47f9503a3603e6ade
bhanumathy <147066713+bhanulk@users.noreply.github.com>
2024-10-31 22:50:30 +0530

    last commit

--- apriori.py
+++ apriori.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from itertools import combinations
 
-# Reading the CSV file
+
 df = pd.read_csv('data.csv')
 
-# Count frequency of itemsets
+
 def generate_item_frequencies(transactions, itemset_size):
     itemcount = {}
     for transaction in transactions:
@@ -16,24 +16,24 @@ def generate_item_frequencies(transactions, itemset_size):
                 itemcount[itemset] = 1
     return itemcount
 
-# Converting into transactions
+
 df['ITEMS'] = df['ITEMS'].apply(lambda x: x.split(','))
 transactions = df['ITEMS'].tolist()
 min_confidence = 0.5
 min_support = 2
 
-# Dictionary to store all frequent itemsets across different sizes
+
 all_frequent_itemsets = {}
 
-# Generate frequent itemsets
+
 for i in range(1, 4):
     item_counts = generate_item_frequencies(transactions, i)
-    # Filter itemsets based on minimum support
+    
     frequent_itemsets = {itemset: count for itemset, count in item_counts.items() if count >= min_support}
     all_frequent_itemsets.update(frequent_itemsets)
     print(f"Frequent {i}-item sets:", frequent_itemsets)
 
-# Generate association rules
+
 association_rules = []
 for itemset in all_frequent_itemsets:
     if len(itemset) > 1:  
@@ -49,6 +49,6 @@ for itemset in all_frequent_itemsets:
                     if confidence >= min_confidence:
                         association_rules.append((antecedent, consequent, confidence))
 
-# Printing association rules
+
 for rule in association_rules:
     print(f"Rule: {rule[0]} -> {rule[1]}, Confidence: {rule[2]:.2f}")
