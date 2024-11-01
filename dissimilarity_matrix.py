584297565ca1b599b71286209bceea07298da419
bhanumathy <147066713+bhanulk@users.noreply.github.com>
2024-11-01 16:16:33 +0530

    commit

--- dissimilarity_matrix.py
+++ dissimilarity_matrix.py
@@ -29,8 +29,37 @@ for i in range (n):
         if i>=j:
             dissimilarity=sum([1 if data[nominal_att].iloc[i,k]!= data[nominal_att].iloc[j,k] else 0 for k in range (len(nominal_att))])
             nom_dismatrix[i][j]=round(dissimilarity,2)
+
+print("nominal attributes dissimilarity matrix")
 for row in nom_dismatrix:
     print(row)
+
+#dissimilarity martix for numeric attributes
+for i in range (n):
+    for j in range (n):
+        if i>=j:
+             dissimilarity=sum([abs(data[numeric_att].iloc[i,k]-data[numeric_att].iloc[j,k])for k in range(len(numeric_att))])
+             num_dismatrix[i][j]=round(dissimilarity,2)
+
+print("numeric attributes dissimilarity matrix")
+for row in num_dismatrix:
+    print(row)
+
+#dissimilarity matrix for mixed attributes
+for i in range(n):
+    for j in range(n):
+        if i>=j:
+            num=num_dismatrix[i][j]/len(numeric_att) if len(numeric_att)>0 else 0
+            nom=nom_dismatrix[i][j]/len(nominal_att) if len(nominal_att)>0 else 0
+            mix_dismatrix[i][j]=round((num+nom)/2,2)
+
+print("mixed attribute dissimilarity matrix")            
+for row in mix_dismatrix:
+    print(row)
+
+
+
+
 
 
 
